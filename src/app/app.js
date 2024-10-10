3e76efd6cc1af1d7b7417add1d466fad175a7b58
Aditya sharma <adsharmavashishtha@gmail.com>
2024-10-10 12:53:56 +0530

    build

--- src/app/app.js
+++ src/app/app.js
@@ -4,12 +4,24 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../config/swagger.json" assert { type: "json" };
 import authRoutes from "../routes/user_routes.js";
 import cors from "cors";
+// CORS configuration
+const corsOptions = {
+  origin: [
+    "http://localhost:4000",
+    "https://https://ak-backend-lime.vercel.app",
+  ], // Allowed origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  credentials: true, // Enable set cookie
+};
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// Enable CORS with options
+app.use(cors(corsOptions));
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes

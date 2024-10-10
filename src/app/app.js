f6066974da6d306dee1a13a72ad24fc958f1ba88
Aditya sharma <adsharmavashishtha@gmail.com>
2024-10-10 12:42:21 +0530

    commit

--- src/app/app.js
+++ src/app/app.js
@@ -7,21 +7,10 @@ import cors from "cors";
 
 const app = express();
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, {
-    customCss:
-      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
-    customCssUrl: CSS_URL,
-  })
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use("/api/auth", authRoutes);
